d72dc13e0391d70055dfbcbce4a9e97ca6133fa5
Alaa Nassar <alaanassar1211@gmail.com>
2023-06-16 21:36:29 +0300

    structure

---
+++ Pets_World_Back-end/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+var cors = require("cors");
+
+////// please don't change anything and use the middlewores //////
+
+//connect to database
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log("Example app listening on port 8080!");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+// first layer logging middleware
+server.use((req, res, next) => {
+  next();
+});
+app.use(express.json());
+app.use(cors());
+app.use("/assets/images", express.static(path.join(__dirname, "")));
+
+//  middelware layers of routing and authentication
+
+//  third layer no page found
+app.use((req, res, next) => {
+  res.status(404).json({ message: "path not found" });
+});
+
+// fourth layer for handling errors
+app.use((err, req, res, next) => {
+  S;
+  res.status(500).json({ message: err + " " });
+});
