a75644f8e67779ef1e155a13e5753523081fb952
Alaa Nassar <alaanassar1211@gmail.com>
2023-06-19 01:43:01 +0300

    edit auth strcuture

--- Pets_World_Front-end/src/app/pages/pages-routing.module.ts
+++ Pets_World_Front-end/src/app/pages/pages-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+
+  {
+    path: '',
+    loadChildren: () =>
+      import('./auth/auth.module').then((register) => register.AuthModule),
+  },
   {
     path: 'user',
     loadChildren: () =>
--- Pets_World_Front-end/src/app/pages/pages.module.ts
+++ Pets_World_Front-end/src/app/pages/pages.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PagesRoutingModule } from './pages-routing.module';
 import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-
 
 @NgModule({
-  declarations: [
-    LandingComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    PagesRoutingModule
-  ]
+  declarations: [LandingComponent],
+  imports: [CommonModule, PagesRoutingModule],
 })
-export class PagesModule { }
+export class PagesModule {}
