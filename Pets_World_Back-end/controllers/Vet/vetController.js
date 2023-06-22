73d4027d289257119eb809f748bc734ee2bf4bef
Alaa Nassar <alaanassar1211@gmail.com>
2023-06-23 02:18:41 +0300

    edit vet

--- Pets_World_Back-end/controllers/Vet/vetController.js
+++ Pets_World_Back-end/controllers/Vet/vetController.js
@@ -1,19 +1,53 @@
 const mongoose = require("mongoose");
 const VetSchema = mongoose.model("Vet");
+const userSchema = mongoose.model("User");
 
 getVetById = async (req, res, next) => {
-    try {
-        const vet = await VetSchema.findOne({
-            _id: req.params.id,
-        }).populate({
-            path: "user_id",
-        });
-        res.status(200).json(vet);
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const vet = await VetSchema.findOne({
+      _id: req.params.id,
+    }).populate({
+      path: "user_id",
+    });
+    res.status(200).json(vet);
+  } catch (err) {
+    next(err);
+  }
 };
-
+updateVetById = async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const vet = await VetSchema.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          experience: req.body.experience,
+          cost: req.body.cost,
+          description: req.body.description,
+        },
+      },
+      { new: true }
+    ).populate({ path: "user_id" });
+    const user = await userSchema.findOneAndUpdate(
+      { _id: vet.user_id },
+      {
+        $set: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          // password: req.body.password,
+          phone: req.body.phone,
+          gender: req.body.gender,
+          // image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json(vet);
+  } catch (err) {
+    next(err);
+  }
+};
 module.exports = {
-    getVetById,
+  getVetById,
+  updateVetById,
 };
--- Pets_World_Back-end/core/Validation/vetValidation.js
+++ Pets_World_Back-end/core/Validation/vetValidation.js
@@ -6,17 +6,45 @@ module.exports.getVetByIdValidator = [
 
 module.exports.addVetValidator = [
   body("user_id").isMongoId().withMessage("that User id isn't Valid"),
-  body("experience").isInt().isLength({ min: 0 }).withMessage("experience should be numeric and 0 or more"),
-  body("cost").isNumeric().isLength({ min: 10 }).withMessage("experience should be numeric and 10 or more"),
-  body("description").optional().isString().withMessage("Description should be a string"),
+  body("experience")
+    .isInt()
+    .isLength({ min: 0 })
+    .withMessage("experience should be numeric and 0 or more"),
+  body("cost")
+    .isNumeric()
+    .isLength({ min: 10 })
+    .withMessage("experience should be numeric and 10 or more"),
+  body("description")
+    .optional()
+    .isString()
+    .withMessage("Description should be a string"),
 ];
 
 module.exports.updateVetValidator = [
-  body("experience").optional().isInt().isLength({ min: 0 }).withMessage("experience should be numeric and 0 or more"),
-  body("cost").optional().isNumeric().isLength({ min: 10 }).withMessage("experience should be numeric and 10 or more"),
-  body("description").optional().isString().withMessage("Description should be a string"),
-  body("numberOfReviews").optional().isInt().isLength({ min: 0 }).withMessage("numberOfReviews should be integer and 0 or more"),
-  body("totalOfReviews").optional().isInt().isLength({ min: 0 }).withMessage("totalOfReviews should be integer and 0 or more"),
+  body("experience")
+    .optional()
+    .isInt()
+    .isLength({ min: 0 })
+    .withMessage("experience should be numeric and 0 or more"),
+  body("cost")
+    .optional()
+    .isInt()
+    .isLength({ min: 10 })
+    .withMessage("cost should be numeric and 10 or more"),
+  body("description")
+    .optional()
+    .isString()
+    .withMessage("Description should be a string"),
+  body("numberOfReviews")
+    .optional()
+    .isInt()
+    .isLength({ min: 0 })
+    .withMessage("numberOfReviews should be integer and 0 or more"),
+  body("totalOfReviews")
+    .optional()
+    .isInt()
+    .isLength({ min: 0 })
+    .withMessage("totalOfReviews should be integer and 0 or more"),
 ];
 
 module.exports.deleteVetValidator = [
