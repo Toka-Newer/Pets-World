401c598d89aa9027a0018dfb1c11fb152e6461cd
unknown <tokanewer1234@gmail.com>
2023-06-24 06:05:27 +0300

    tasks

--- Pets_World_Back-end/controllers/userController.js
+++ Pets_World_Back-end/controllers/userController.js
@@ -15,9 +15,8 @@ addUser = async (req, res, next) => {
         phone: req.body.phone,
         role: req.body.role,
         gender: req.body.gender,
-        // ...(req.file && { image: req.file.path }), // Include the image field
       });
-      // Handle the license image
+
       if (req.files && req.files.length > 0) {
         // const userImages = req.files.filter(
         //   (file) => file.fieldname === "image"
@@ -162,67 +161,3 @@ addUser = async (req, res, next) => {
 module.exports = {
   addUser,
 };
-
-// to add more than one pet handel in front first
-// addUser = async (req, res, next) => {
-//   try {
-//     if (req.body.retypePassword === req.body.password) {
-//       const newUser = new userSchema({
-//         firstName: req.body.firstName,
-//         lastName: req.body.lastName,
-//         email: req.body.email,
-//         password: req.body.password,
-//         phone: req.body.phone,
-//         role: req.body.role,
-//         gender: req.body.gender,
-//       });
-
-//       // ... existing code ...
-
-//       const savedUser = await newUser.save();
-
-//       if (req.body.role === "owner") {
-//         const pets = req.body.pets; // Assume an array of pets is sent in the request body
-
-//         const petPromises = pets.map(async (pet) => {
-//           const newPet = new PetsSchema({
-//             name: pet.name,
-//             type: pet.type,
-//             gender: pet.gender,
-//             dateOfBirth: pet.dateOfBirth,
-//             age: pet.age,
-//             description: pet.description,
-//           });
-
-//           await newPet.save();
-
-//           return {
-//             pet_id: newPet._id,
-//           };
-//         });
-
-//         const petData = await Promise.all(petPromises);
-
-//         const owner = new OwnerSchema({
-//           user_id: savedUser._id,
-//           pets: petData,
-//         });
-
-//         await owner.save();
-//       } else {
-//         // ... existing code ...
-//       }
-
-//       // Return a success response
-//       return res.status(201).json({ data: savedUser });
-//     } else {
-//       // Passwords do not match
-//       const error = new Error("Passwords do not match.");
-//       error.status = 400;
-//       throw error;
-//     }
-//   } catch (error) {
-//     // Pass the error to the error-handling middleware
-//     next(error);
-//   }
-// };
