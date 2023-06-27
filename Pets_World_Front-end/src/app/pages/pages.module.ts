4c32f8a2a6b3f6397f70bbfc544c2e382a6b9d1c
Alaa Nassar <alaanassar1211@gmail.com>
2023-06-27 05:52:55 +0300

    landing

--- Pets_World_Front-end/src/app/pages/pages.module.ts
+++ Pets_World_Front-end/src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { LandingComponent } from './landing/landing.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule, PagesRoutingModule],
+  imports: [CommonModule, PagesRoutingModule, CarouselModule],
 })
 export class PagesModule {}
