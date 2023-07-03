7797378ba56d13fe48a8f96f8711d9778e52ea10
unknown <ahmedhimida4321@gmail.com>
2023-07-03 04:42:03 +0300

    add role subject and subscribe to the value

--- Pets_World_Front-end/src/app/app.component.ts
+++ Pets_World_Front-end/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, SimpleChanges } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 import { Router } from '@angular/router';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pets_World_Front-end';
+  roleObservable: Observable<string> = this.authService.roleSubject.asObservable();
   role: any = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
   ngOnInit() {
     this.authService.getTokenData();
-    this.role = this.authService.role;
+    this.roleObservable.subscribe(role => {
+      this.role = role;
+    })
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.role = changes['role'];
-    console.log(this.role);
-  }
+  // ngOnChanges(changes: SimpleChanges) {
+  //   this.role = changes['role'];
+  //   console.log(this.role);
+  // }
 
   logout() {
     this.authService.logout();
