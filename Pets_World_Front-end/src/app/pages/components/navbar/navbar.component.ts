4afe220f9df3f15edd0100902d458f5883c5835d
Alaa Nassar <alaanassar1211@gmail.com>
2023-07-03 07:59:46 +0300

    login

--- Pets_World_Front-end/src/app/pages/components/navbar/navbar.component.ts
+++ Pets_World_Front-end/src/app/pages/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -8,6 +9,8 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  roleObservable: Observable<string> =
+    this.authService.roleSubject.asObservable();
   role: any = '';
 
   constructor(private authService: AuthService, private router: Router) {}
@@ -15,11 +18,9 @@ export class NavbarComponent {
   ngOnInit() {
     this.authService.getTokenData();
     this.role = this.authService.role;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    this.role = changes['role'];
-    console.log(this.role);
+    this.roleObservable.subscribe((role) => {
+      this.role = role;
+    });
   }
 
   logout() {
